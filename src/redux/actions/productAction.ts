133a8fd540fc9c9ad5546145db22b76e7309c332
siwat jermwatthana <siwat.j@hiveground.com>
2023-12-11 17:30:31 +0700

    [Fix]: Refactor productAction.ts to use 'name' instead of 'keyword' in getProducts and add getProductByIds

--- src/redux/actions/productAction.ts
+++ src/redux/actions/productAction.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { productService } from "@/services/productAPI";
 import { store } from "../store";
 
-export const getProducts = createAsyncThunk("product/get", async (options: { keyword?: string; limit: number; page: number }) => {
+export const getProducts = createAsyncThunk("product/get", async (options: { name?: string; limit: number; page: number }) => {
   const productList = await productService.getProducts(options);
   const productWithImageBlob = [];
   for (const product of productList.data) {
@@ -21,6 +21,25 @@ export const getProducts = createAsyncThunk("product/get", async (options: { key
     total: productList.total,
     limit: productList.limit,
     page: productList.page,
+  };
+});
+
+export const getProductByIds = createAsyncThunk("product/getByIds", async (params: string[]) => {
+  const productList = await productService.getProductByIds(params);
+  const productWithImageBlob = [];
+  for (const product of productList) {
+    if (product.image) {
+      const arrayBuffer = await productService.getImageProduct(product.image);
+      const blob = new Blob([arrayBuffer]);
+      const objectUrl = URL.createObjectURL(blob);
+      product.image = objectUrl;
+    }
+    productWithImageBlob.push({
+      ...product,
+    });
+  }
+  return {
+    data: productWithImageBlob,
   };
 });
 
