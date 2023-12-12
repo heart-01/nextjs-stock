78f6327c997af3ee11d8eccd7dd90f7eac36abf5
siwat jermwatthana <siwat.j@hiveground.com>
2023-12-12 20:29:28 +0700

    [Fix]: Update cookie settings in sign-in API

--- src/pages/api/auth/[...nextAuth].ts
+++ src/pages/api/auth/[...nextAuth].ts
@@ -26,10 +26,11 @@ const signIn = async (req: NextApiRequest, res: NextApiResponse) => {
     // set token to cookie
     const { token, refreshToken } = response.data;
     setCookie(res, [ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY], [token, refreshToken], {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== "development",
-      sameSite: "strict",
-      path: "/",
+      httpOnly: true, // Specifies that cookies must be set only from the server side.
+      secure: process.env.NODE_ENV !== "development", // Specifies for the browser to only send the cookie over HTTPS.
+      sameSite: "strict", // Not allow browsers to send cookies across websites.
+      maxAge: 7 * 24 * 60 * 60,  // expires in 7 days
+      path: "/",  // Store the cookie using for all routes
     });
 
     res.json(response.data);
