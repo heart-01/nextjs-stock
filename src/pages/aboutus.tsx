baef2cae65e51860143a0060c9ed1eb86c3dc923
siwat jermwatthana <siwat.j@hiveground.com>
2024-01-21 16:11:23 +0700

    [Feat]: Add page about iframe

--- src/pages/aboutus.tsx
+++ src/pages/aboutus.tsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { Paper } from "@mui/material";
+import Iframe from "react-iframe";
 import Layout from "@/components/layouts/Layout";
+import withAuth from "@/hoc/withAuth";
 
 type Props = {};
 
-const Aboutus = (props: Props) => {
+const AboutUs = ({}: Props) => {
   return (
     <Layout>
-      <div>aboutus</div>
+      <Paper sx={{ height: "86vh" }}>
+        <Iframe url="https://example.com" width="100%" height="100%" id="myId" display="inline" position="relative" />
+      </Paper>
     </Layout>
   );
 };
 
-export default Aboutus;
+export default withAuth(AboutUs);
